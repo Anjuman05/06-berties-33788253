136edab5419f612731692e2c7c2b7341e33aad00
aisla011 <aisla011@gold.ac.uk>
2025-11-13 20:52:48 +0000

    search function complete

--- routes/main.js
+++ routes/main.js
@@ -11,5 +11,26 @@ router.get('/about',function(req, res, next){
     res.render('about.ejs')
 });
 
+router.get('/search', function(req, res, next){
+    res.render("search.ejs");
+});
+
+router.get('/search-result', function (req, res, next){
+    const searchKeyword = req.query.search_text;
+
+    let sqlquery = "SELECT * FROM books WHERE name LIKE ?";
+    let searchValue = '%' + searchKeyword + '%';
+
+    db.query(sqlquery, [searchValue], (err, result) => {
+        if(err){
+            next(err);
+        }
+        res.render("search-result.ejs", {
+            books: result,
+            keyword: searchKeyword
+        });
+    });
+});
+
 // Export the router object so index.js can access it
 module.exports = router
