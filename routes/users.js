fdceb5a936f00f5aa7ca7e31c7a284275a815d2c
aisla011 <aisla011@gold.ac.uk>
2025-11-20 21:09:53 +0000

    users registered page

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,28 @@
 // Create a new router
 const express = require("express")
+const bcrypt = require("bcrypt")
 const router = express.Router()
+const saltRounds = 10
 
 router.get('/register', function (req, res, next) {
     res.render('register.ejs')
 })
 
 router.post('/registered', function (req, res, next) {
-    // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
+    const plainPassword = req.body.password
+    const {username, first, last, email} = req.body;
+    
+    bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
+    // Store hashed password in your database.
+    let sqlquery = "INSERT INTO users( username, first_name, last_name, email, hashedPassword) Values (? ,? ,? ,? ,?)";
+    let newrecord = [username, first, last, email, hashedPassword]; 
+    
+        db.query(sqlquery, newrecord, (err, result) => { 
+            let resultMessage = 'Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email;
+            resultMessage += 'Your password is: '+ req.body.password +' and your hashed password is: '+ hashedPassword;
+            res.send(resultMessage);
+        });
+    });                                                                  
 }); 
 
 // Export the router object so index.js can access it
