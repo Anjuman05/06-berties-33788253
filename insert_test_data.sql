2d8d6800bc9b616e0aae25e88c1044ef9f8afe8b
aisla011 <aisla011@gold.ac.uk>
2025-11-28 02:05:01 +0000

    register user gold

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,3 +3,4 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+INSERT INTO users(username, first_name, last_name, email, hashedPassword) VALUES ('gold', 'Gold', 'Smiths', 'gold@email.com', '$2b$10$MbHdWxCrZ3BZTZn398EzpOCTeqlQH/B1C00zjpyS/Ygti9GoSjgUu');
