c1c2d3f73bdc9ba367e04745bc5cbc1aa7c4b2ce
aisla011 <aisla011@gold.ac.uk>
2025-12-07 03:23:18 +0000

    attempt at search

--- routes/api.js
+++ routes/api.js
@@ -6,9 +6,18 @@ router.get('/books', function (req, res, next) {
 
     // Query database to get all the books
     let sqlquery = "SELECT * FROM books"
+    let search = req.query.search;
+
+    if(search){
+        sqlquery = "SELECT * FROM books WHERE name LIKE ?";
+        search = '%' + search +'%'
+    }
+    else{
+        sqlquery = "SELECT * FROM books"
+    }
 
     // Execute the sql query
-    db.query(sqlquery, (err, result) => {
+    db.query(sqlquery, [search] (err, result) => {
         // Return results as a JSON object
         if (err) {
             res.json(err)
